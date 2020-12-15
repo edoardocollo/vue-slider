0033107ec35892ba22f6052a2bb508a758419f1f
edoardocollo <edoardo.collo@gmail.com>
2020-12-15 23:31:55 +0100

    new version

--- main.js
+++ main.js
@@ -1,56 +1,30 @@
 let app = new Vue({
   el: '#app',
   data: {
-    img: ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg'],
-    selectedImg: '1.jpg',
+    img: [
+      'img/1.jpg',
+      'img/2.jpg',
+      'img/3.jpg',
+      'img/4.jpg',
+      'img/5.jpg'
+    ],
+    index: 0,
   },
   methods: {
     next: function(){
-      let currentIndex = this.img.indexOf(this.selectedImg);
-      if (currentIndex != (this.img.length - 1)) {
-        currentIndex++;
-        this.selectedImg = this.img[currentIndex];
-      }else{
-        currentIndex = 0;
-        this.selectedImg = this.img[currentIndex];
+      if (this.index == this.img.length - 1) {
+        this.index = 0;
+      }else {
+        this.index++;
       }
-      console.log(currentIndex);
-      const target = document.querySelectorAll('.pointer');
-      target.forEach(e =>{
-        e.className = 'pointer'
-      });
-      target[this.img.indexOf(this.selectedImg)].className += ' active';
-
     },
     previous: function(){
-      let currentIndex = this.img.indexOf(this.selectedImg);
-      if (currentIndex == 0) {
-        currentIndex = (this.img.length - 1);
-        this.selectedImg = this.img[currentIndex];
-      }else{
-        currentIndex--;
-        this.selectedImg = this.img[currentIndex];
+      if (this.index == 0) {
+        this.index = this.img.length - 1;
+      }else {
+        this.index--;
       }
-      console.log(currentIndex);
+    }
 
-      const target = document.querySelectorAll('.pointer');
-      target.forEach(e =>{
-        e.className = 'pointer'
-      });
-      target[this.img.indexOf(this.selectedImg)].className += ' active';
-
-    },
-    pointerClick: function(index){
-      this.selectedImg = this.img[index];
-
-      const target = document.querySelectorAll('.pointer');
-      target.forEach(e =>{
-        e.className = 'pointer'
-      });
-      target[index].className += ' active'
-    },
-  },
-  created() {
-      this.interval = setInterval(() => this.next(), 6000);
-  },
+  }
 });
