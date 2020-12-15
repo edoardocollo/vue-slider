6d3b02859c7363756d504517eda06a943cb86412
edoardocollo <edoardo.collo@gmail.com>
2020-12-15 13:27:48 +0100

    created html structure, option object and methods

---
+++ main.js
@@ -0,0 +1,32 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+    img: ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg'],
+    selectedImg: '1.jpg',
+  },
+  methods: {
+    next: function(){
+      let currentIndex = this.img.indexOf(this.selectedImg);
+      if (currentIndex != (this.img.length - 1)) {
+        currentIndex++;
+        this.selectedImg = this.img[currentIndex];
+      }else{
+        currentIndex = 0;
+        this.selectedImg = this.img[currentIndex];
+      }
+      console.log(currentIndex);
+    },
+    previous: function(){
+      let currentIndex = this.img.indexOf(this.selectedImg);
+      if (currentIndex == 0) {
+        currentIndex = (this.img.length - 1);
+        this.selectedImg = this.img[currentIndex];
+      }else{
+        currentIndex--;
+        this.selectedImg = this.img[currentIndex];
+      }
+      console.log(currentIndex);
+    },
+
+  },
+});
