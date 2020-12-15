a3688f60f0931f4f80b93c9454fa0cbcfc865823
edoardocollo <edoardo.collo@gmail.com>
2020-12-16 00:18:54 +0100

    added autoplay

--- main.js
+++ main.js
@@ -9,22 +9,48 @@ let app = new Vue({
       'img/5.jpg'
     ],
     index: 0,
+
   },
+  mounted(){
+
+    this.printActive();
+
+  },
+
   methods: {
-    next: function(){
+    next(){
       if (this.index == this.img.length - 1) {
         this.index = 0;
       }else {
         this.index++;
       }
+      this.printActive();
     },
-    previous: function(){
+    previous(){
       if (this.index == 0) {
         this.index = this.img.length - 1;
       }else {
         this.index--;
       }
+      this.printActive();
+    },
+    autoSlide(){
+      this.timer = setInterval(this.next, 500);
+    },
+    pointerClick(indexPointer){
+      this.index = indexPointer;
+      this.printActive();
+    },
+    printActive(){
+      const target = document.querySelectorAll('.pointer');
+      target.forEach(e=>{
+        e.className = 'pointer';
+      });
+      target[this.index].className += ' active';
     }
 
-  }
+  },
+  created(){
+    this.timer = setInterval(this.next, 6000);
+  },
 });
